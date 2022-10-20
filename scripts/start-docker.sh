33a4c97d180e3afcaf464cebef2cebb645394e37
Mario Karagiorgas <salem8@gmail.com>
2022-10-21 01:43:43 +0300

    Upgrade start-docker in sync with Evmos

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
-./marad validate-genesis --home /mara
+KEY="mykey"
+CHAINID="mara_9000-1"
+MONIKER="mymoniker"
+DATA_DIR=$(mktemp -d -t mara-datadir.XXXXX)
 
-echo "starting mara node $ID in background ..."
-./marad start \
---home /mara \
---keyring-backend test
+echo "create and add new keys"
+./marad keys add $KEY --home $DATA_DIR --no-backup --chain-id $CHAINID --algo "eth_secp256k1" --keyring-backend test
+echo "init Evmos with moniker=$MONIKER and chain-id=$CHAINID"
+./marad init $MONIKER --chain-id $CHAINID --home $DATA_DIR
+echo "prepare genesis: Allocate genesis accounts"
+./marad add-genesis-account \
+"$(./marad keys show $KEY -a --home $DATA_DIR --keyring-backend test)" 1000000000000000000amara,1000000000000000000stake \
+--home $DATA_DIR --keyring-backend test
+echo "prepare genesis: Sign genesis transaction"
+./marad gentx $KEY 1000000000000000000stake --keyring-backend test --home $DATA_DIR --keyring-backend test --chain-id $CHAINID
+echo "prepare genesis: Collect genesis tx"
+./marad collect-gentxs --home $DATA_DIR
+echo "prepare genesis: Run validate-genesis to ensure everything worked and that the genesis file is setup correctly"
+./marad validate-genesis --home $DATA_DIR
+
+echo "starting mara node $i in background ..."
+./marad start --pruning=nothing --rpc.unsafe \
+--keyring-backend test --home $DATA_DIR \
+>$DATA_DIR/node.log 2>&1 & disown
 
 echo "started mara node"
 tail -f /dev/null
