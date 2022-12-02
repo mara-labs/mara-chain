bb1578dbd7ed5f78a49afad63798a6437bd343b4
Mario Karagiorgas <blewater@users.noreply.github.com>
2022-12-02 20:16:40 +0200

    Replace ethermint keeper with Mara

--- app/ante/eth.go
+++ app/ante/eth.go
@@ -10,9 +10,10 @@ import (
 	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
 
 	ethermint "github.com/evmos/ethermint/types"
-	evmkeeper "github.com/evmos/ethermint/x/evm/keeper"
 	"github.com/evmos/ethermint/x/evm/statedb"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
+
+	evmkeeper "github.com/mara-labs/mara-chain/x/evm/keeper"
 
 	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
