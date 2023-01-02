5e06d6a7ae1d8897d0fc7d8f65417821bf4d007b
Donald Geddes <hi@donaldgeddes.ca>
2023-01-02 13:25:36 -0800

    Add basic config
    * Basic `config.js` including sidebar structure
    * Inclusion of Mara logo
    * Very basic style customizations

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  theme: 'cosmos',
+  title: 'Mara Documentation',
+  themeConfig: {
+    logo: {
+      src: '/logo.svg',
+    },
+    topbar: {
+      banner: false,
+    },
+    custom: true,
+    sidebar: {
+      auto: false,
+      nav: [
+        {
+          title: 'About Mara',
+          children: [
+            {
+              title: 'Introduction',
+              directory: true,
+              path: '/intro',
+            },
+          ],
+        },
+        {
+          title: 'For Validators',
+          children: [
+            {
+              title: 'Overview',
+              directory: false,
+              path: '/validators/overview',
+            },
+            {
+              title: 'Installation',
+              directory: false,
+              path: '/validators/installation',
+            },
+            {
+              title: 'Setup & Configuration',
+              directory: true,
+              path: '/validators/setup',
+            },
+            {
+              title: 'Join Testnet',
+              directory: false,
+              path: '/validators/join-testnet',
+            },
+            {
+              title: 'Join Mainnet',
+              directory: false,
+              path: '/validators/join-mainnet',
+            },
+            {
+              title: 'Security',
+              directory: true,
+              path: '/validators/security',
+            },
+            {
+              title: 'Software Upgrades',
+              directory: true,
+              path: '/validators/software-upgrades',
+            },
+            {
+              title: 'Frequently Asked Questions',
+              directory: false,
+              path: '/validators/faq',
+            },
+          ],
+        },
+      ],
+    },
+  },
+}
